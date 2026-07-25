afb69f3d7b4d14b79c922b7b5800b12a86ff0974
Fajle Rabbi <fajler093@gmail.com>
2026-07-26 00:03:58 +0600

    add

---
+++ decent_arrays.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{ 
+    int N;
+    int arr[100];
+    int flag = 1;
+   
+    scanf("%d",&N);
+
+    for(int i=0; i < N; i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i = 1; i < N; i++){
+        if(arr[i]<arr[i-1]){
+            flag = 0;
+            break;
+        }
+    }
+        if(flag == 1){
+            printf("Yes\n");
+        }else{
+            printf("No\n");
+        }
+    return 0;
+}
